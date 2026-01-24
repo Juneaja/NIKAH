1e7c7307a56cf4be4db3368361a644cd1f7be0c4
ngoding asik <113892656+Juneaja@users.noreply.github.com>
2026-01-24 16:12:39 +0700

    Update message.js

--- message.js
+++ message.js
@@ -1,12 +1,11 @@
-const scriptURLPost = "https://script.google.com/macros/s/AKfycbwzglfQcqwTZlCRClA656ks4OpLt98XKhuPRIunzbi77f6Ci98pFtpONHGd5sugykNz/exec";
+const scriptURLPost = "https://script.google.com/macros/s/AKfycbwM2NNaNRqRcZJMGFltgCRoO3DSc_fjW9s2kXBPYqTRhRxqWd9oHu-ny4GlZOE9taJ4/exec";
 const form = document.forms["titip-pesan"];
 const btnKirim = document.querySelector(".btn-kirim");
 const btnLoading = document.querySelector(".btn-loading");
 const myAlert = document.querySelector(".alert-warning");
 const container = document.querySelector("#message-box");
 const scriptURLGet =
-  "https://script.googleusercontent.com/macros/echo?user_content_key=S1QLM2jFCK5K1fZDleY8cQucE4ZvJ44eyyYQIkx2h-L_IxGnypXjeaeG4hnA12bfijqBvKSM9AEMUzfa0gz__NGmPU6JkrT8m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDKBGPRyCERUhrNvBLE_vI2yf5xa67PjQRYQDOhZ_4SyL7VwKS3eeoLwYhAKqpow5c3jCVF_JdC15C13WLs7us0bBZmdxEO81tz9Jw9Md8uu&lib=MsW59pOlHEHYIZnQl7KkJZD5VydodrvMG";
-
+  "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLgSjCbD5X8b631EmkGBEVr4AyXJKs_lORHKv9DmyWu5oUBozE9zQifpd4SKthv9qTlYEXXzJSyRyrJ2PxLCJTa3HdsrlQLsHk1Y26gYHZq_9q8D-lVueirVwGIHzcEdYzdQH-YhmrNBd3K5xlTYuN5MOc9S_yKcfVoaWOlfi17dqNsOoAwG-eYymfknIRVLB6BseZYgV43VciFQxxysC74J-qGaBIRbYP2IASQbQMPeMPQacLF0vaYunKXvH3-TiKkPN7AKYzCrqkBoYunYrH_2Q-gjYQ&lib=MXmdWRyV0bgPKdDboftv77IQAT1r8vRmO";
 fetchMessage();
 
 form.addEventListener("submit", (e) => {
@@ -55,3 +54,4 @@ function maker(data) {
   });
   container.innerHTML = content;
 }
+
