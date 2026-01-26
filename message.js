5114eff02c454005a380dd6340a3ce50b34e0c30
ngoding asik <113892656+Juneaja@users.noreply.github.com>
2026-01-26 11:51:20 +0700

    Update message.js

--- message.js
+++ message.js
@@ -1,11 +1,10 @@
-const scriptURLPost = "https://script.google.com/macros/s/AKfycbwzglfQcqwTZlCRClA656ks4OpLt98XKhuPRIunzbi77f6Ci98pFtpONHGd5sugykNz/exec";
+const scriptURLPost = "https://script.google.com/macros/s/AKfycbzURUrZYt0Tv9NXzFtkMB5Q4M7VTgc7JWvad46tXpC4ShkNk_e4q_us0zulRYXOG6D3/exec";
 const form = document.forms["titip-pesan"];
 const btnKirim = document.querySelector(".btn-kirim");
 const btnLoading = document.querySelector(".btn-loading");
 const myAlert = document.querySelector(".alert-warning");
 const container = document.querySelector("#message-box");
-const scriptURLGet =
-  "https://script.googleusercontent.com/macros/echo?user_content_key=S1QLM2jFCK5K1fZDleY8cQucE4ZvJ44eyyYQIkx2h-L_IxGnypXjeaeG4hnA12bfijqBvKSM9AEMUzfa0gz__NGmPU6JkrT8m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDKBGPRyCERUhrNvBLE_vI2yf5xa67PjQRYQDOhZ_4SyL7VwKS3eeoLwYhAKqpow5c3jCVF_JdC15C13WLs7us0bBZmdxEO81tz9Jw9Md8uu&lib=MsW59pOlHEHYIZnQl7KkJZD5VydodrvMG";
+const scriptURLGet = "https://script.google.com/macros/s/AKfycbzURUrZYt0Tv9NXzFtkMB5Q4M7VTgc7JWvad46tXpC4ShkNk_e4q_us0zulRYXOG6D3/exec";
 
 fetchMessage();
 
@@ -55,3 +54,4 @@ function maker(data) {
   });
   container.innerHTML = content;
 }
+
